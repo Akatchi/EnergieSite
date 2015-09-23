53d7aede50070b541462e833897f50c7774ebe31
Vasco de Krijger <vascodekrijger@gmail.com>
2015-09-23 20:31:21 +0200

    Basic db setup

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,24 @@
 package controllers
 
+import javax.inject.Inject
+
+import dao.UserDAO
+import models.User
 import play.api._
 import play.api.mvc._
 
-class Application extends Controller {
+class Application @Inject() (userDao: UserDAO) extends Controller {
 
   def index = Action {
+
+    val user = new User("pietje", "red")
+
+    userDao.insert(user)
+
+    Logger.debug(user.name)
+
+    val users = userDao.all()
+
     Ok(views.html.index("Your new application is ready."))
   }
 
---
+++ target/scala-2.11/routes/main/router/RoutesPrefix.scala
@@ -0,0 +1,16 @@
+
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/home/akatchi/Dropbox/Libraries/Playframework/activator-dist-1.3.6/energie_site/conf/routes
+// @DATE:Wed Sep 23 20:05:12 CEST 2015
+
+
+package router {
+  object RoutesPrefix {
+    private var _prefix: String = "/"
+    def setPrefix(p: String): Unit = {
+      _prefix = p
+    }
+    def prefix: String = _prefix
+    val byNamePrefix: Function0[String] = { () => prefix }
+  }
+}
